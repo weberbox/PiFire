40c954cfd0676c19be58f549a54bca2a1411c409
James <313328+weberbox@users.noreply.github.com>
2021-11-19 23:04:41 -0800

    Updated the Auto-Install and Modules Configuration scripts to include settings for relay trigger levels and button levels.
    Removed duplication of modules configuration in the auto-install script, and going forward all module settings will use the modules.sh bash script instead.
    
    In preparation for the merge of development features back into main, I've added a backup / restore capability (in the admin panel) for both the settings and the pelletdb JSON files.  There is a local backup directory /backups where files are stored locally, but files can also be downloaded and restored from remote backups (via upload).
    
    The readme.md has been updated to include newer pictures, updated links, a new video and a features list.  Again this is all in anticipation for merging development changes back into the main.
    
    The install.sh script has been updated to do a shallow clone of PiFire so that most users aren't downloading the entire history and bulk of the repository, and only getting the latest.
    
    This version requires flask v2.x due to a change in the file upload capability.  If your install is on an older version, use 'pip3 install --upgrade flask' to update to the latest.
    
    A couple of minor bugfixes are included.

--- common.py
+++ common.py
@@ -419,7 +419,7 @@ def WriteControl(control):
 	cmdsts.set('control:general', json.dumps(control))
 
 
-def ReadSettings():
+def ReadSettings(filename='settings.json'):
 	# *****************************************
 	# Read Settings from file
 	# *****************************************
@@ -428,7 +428,7 @@ def ReadSettings():
 	settings = DefaultSettings()
 
 	try:
-		json_data_file = os.fdopen(os.open('settings.json', os.O_RDONLY))
+		json_data_file = os.fdopen(os.open(filename, os.O_RDONLY))
 		#json_data_file = open("settings.json", "r")
 		json_data_string = json_data_file.read()
 		settings_struct = json.loads(json_data_string)
@@ -446,7 +446,7 @@ def ReadSettings():
 		WriteLog(event)
 		json_data_file.close()
 		# Retry Reading Settings
-		settings_struct = ReadSettings() 
+		settings_struct = ReadSettings(filename=filename)
 
 	# Overlay the read values over the top of the default settings
 	#  This ensures that any NEW fields are captured.  
@@ -459,9 +459,9 @@ def ReadSettings():
 					update_settings = True
 			settings[key].update(settings_struct.get(key, {}))
 		else: 
-			update_settings = True 
+			update_settings = True
 
-	if update_settings: # If any of the keys were added, then write back the changes 
+	if (update_settings) or (filename != 'settings.json'): # If any of the keys were added, then write back the changes
 		WriteSettings(settings)
 		#print('key mismatch - update flag set')
 	
@@ -504,21 +504,38 @@ def WriteRecipes(recipes):
 	with open("recipes.json", 'w') as recipes_file:
 		recipes_file.write(json_data_string)
 
-def ReadPelletDB():
+def ReadPelletDB(filename='pelletdb.json'):
 	# *****************************************
 	# Read Pellet DataBase from file
 	# *****************************************
 
+	pelletdb = DefaultPellets()
+
 	# Read all lines of pelletdb.json into an list(array)
 	try:
-		json_data_file = os.fdopen(os.open('pelletdb.json', os.O_RDONLY))
+		json_data_file = os.fdopen(os.open(filename, os.O_RDONLY))
 		#json_data_file = open("pelletdb.json", "r")
 		json_data_string = json_data_file.read()
-		pelletdb = json.loads(json_data_string)
+		pelletdb_struct = json.loads(json_data_string)
 		json_data_file.close()
 	except(IOError, OSError):
 		# Issue with reading JSON, so create one/write new one
 		pelletdb = DefaultPellets()
+		WritePelletDB(pelletdb)
+		return(pelletdb)
+
+	# Overlay the read values over the top of the default values
+	#  This ensures that any NEW fields are captured.
+	update_db = False # set flag in case an update needs to be written back
+
+	for key in pelletdb.keys():
+		if key in pelletdb_struct.keys():
+			pelletdb[key] = pelletdb_struct[key].copy()
+		else:
+			update_db = True
+
+		# If any of the keys were added or if restoring from file, then write back the changes
+	if (update_db) or (filename != 'pelletdb.json'):
 		WritePelletDB(pelletdb)
 
 	return(pelletdb)
